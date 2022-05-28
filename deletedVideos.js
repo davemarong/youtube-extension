a6a4c381c779419fde9f79bb0e040259235037c4
davemarong <kmarong@hotmail.com>
2022-05-28 13:03:39 +0200

    Add showDeletedVideos functionality

--- deletedVideos.js
+++ deletedVideos.js
@@ -2,19 +2,21 @@ let deletedVideos_list = document.querySelector(".deletedVideos_list");
 let deletedVideos_button = document.querySelector(".deletedVideos_button");
 
 const toggleDeletedVideos = () => {
-  chrome.storage.sync.get("data", ({ data }) => {
-    const { deletedVideos } = data;
-    console.log("from dele†e", deletedVideos);
-    for (let i = 0; i < deletedVideos.length; i++) {
-      deletedVideos_list.innerHTML += `
-      <li>
-        ${deletedVideos[i]}
-      </li>
-      `;
-    }
-  });
-  //   deletedVideos_list.innerHTML = "Hei min venn";
-  console.log("delete");
+  if (deletedVideos_list.innerHTML) {
+    deletedVideos_list.innerHTML = "";
+  } else {
+    chrome.storage.sync.get("data", ({ data }) => {
+      const { deletedVideos } = data;
+      console.log("from delete", deletedVideos);
+      for (let i = 0; i < deletedVideos.length; i++) {
+        deletedVideos_list.innerHTML += `
+              <li>
+              ${deletedVideos[i]}
+              </li>
+              `;
+      }
+    });
+  }
 };
 
 deletedVideos_button.addEventListener("click", toggleDeletedVideos);
